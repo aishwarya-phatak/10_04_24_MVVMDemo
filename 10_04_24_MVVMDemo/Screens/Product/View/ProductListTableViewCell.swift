ac8877c970776dc7036ac0d95f08f8e979aace48
aishwarya <ashp17star@gmail.com>
2024-06-19 11:13:45 +0530

    MVVM Day 2

---
+++ 10_04_24_MVVMDemo/Screens/Product/View/ProductListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductListTableViewCell.swift
+//  10_04_24_MVVMDemo
+//
+//  Created by Vishal Jagtap on 19/06/24.
+//
+
+import UIKit
+
+class ProductListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 10_04_24_MVVMDemo/Screens/Product/View/ProductListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductListViewController.swift
+//  10_04_24_MVVMDemo
+//
+//  Created by Vishal Jagtap on 19/06/24.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
